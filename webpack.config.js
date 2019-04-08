14ebaf2940679911815f51b21e91902d5b832e27
carpenter-js <carpenterbarrett@gmail.com>
2019-04-08 14:25:24 -0600

    css

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,14 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: 'babel-loader'
         }
-      }
+      },
+      {
+        test: /.css$/,
+        exclude: [/node_modules/],
+        use: ['style-loader', 'css-loader']
+      },
     ]
   }
 };
